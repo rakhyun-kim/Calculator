2551ae4cd21a5dda7d8f93ea170f419953eb310a
RAKHYUN <kyle2598@gmail.com>
2023-10-11 12:06:05 -0500

    - Update SunMoonView, adding light mode and drak mode

--- Calculator/SunMoonView.swift
+++ Calculator/SunMoonView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct SunMoonView: View {
     var lightMode: Bool
     var body: some View {
-        Text("SunMoonView!")
+        HStack(spacing: 30) {
+            Image(systemName: "sun.min")
+                .imageScale(.large)
+                .foregroundStyle(lightMode ? sunOrMoonSelectedColor : sunOrMoonNotSelectedColor)
+            Image(systemName: "moon")
+                .imageScale(.large)
+                .foregroundStyle(lightMode ? sunOrMoonNotSelectedColor : sunOrMoonSelectedColor)
+        }
+        .padding()
+        .background(secondaryBackgroundColor)
+        .cornerRadius(20)
     }
 }
 
 #Preview {
-    SunMoonView(lightMode: true)
+    VStack {
+        SunMoonView(lightMode: true)
+        SunMoonView(lightMode: false)
+    }
 }
