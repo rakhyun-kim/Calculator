3be89188e214936083c72213af568dc468dcb533
RAKHYUN <kyle2598@gmail.com>
2023-10-12 11:08:48 -0500

    - Add String helper func

---
+++ Calculator/StringHelperFunctions.swift
@@ -0,0 +1,28 @@
+//
+//  StringHelperFunctions.swift
+//  Calculator
+//
+//  Created by Rakhyun Kim on 10/12/23.
+//
+
+import Foundation
+
+/// Returns last character if it exist
+/// Otherwise return an empty string
+func getLastChar(str: String) -> String {
+    return str.isEmpty ? "" : String(str.last!)
+}
+
+/// Return true if last character of str is equal to char
+func lastCharacterISEqualTo(str: String, char:String) -> Bool {
+    let last = getLastChar(str: str)
+    return last == char
+}
+
+func formatResult(val: Double) -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .decimal
+    numberFormatter.maximumFractionDigits = 16
+
+    return numberFormatter.string(from: NSNumber(value: val)) ?? "0"
+}
